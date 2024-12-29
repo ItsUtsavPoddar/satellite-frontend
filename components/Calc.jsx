39545e9cfbba8dc016cef79bdde47147c7e7c0a9
Utsav Poddar <utsavpoddar002@gmail.com>
2024-12-29 18:52:11 +0530

    3.1.1 Satellite Passes feature added, UI addition soon

--- components/Calc.jsx
+++ components/Calc.jsx
@@ -4,50 +4,116 @@ import { useDispatch, useSelector } from "react-redux";
 import { satCoordsUpdated, satPathUpdated } from "@/redux/slices/satData";
 import { useEffect } from "react";
 const satellite = require("satellite.js");
+let positionEcf = null;
 
 const Calc = ({ satNum }) => {
   const dispatch = useDispatch();
   const satellites = useSelector((state) => state.satDataReducer[satNum]);
+  const observerGd = {
+    longitude: satellite.degreesToRadians(85.7956695),
+    latitude: satellite.degreesToRadians(20.2516902),
+    height: 0.06,
+  };
 
   const cords = (line1, line2) => {
-    // console.log(line1, "\n", line2);
-    // console.log(typeof line1, "\n", typeof line2);
+    // Return some default value or handle the error appropriately already handled when fething still a fallback
     if (!line1 || !line2) {
       console.error("TLE data is missing:", line1, line2);
-      return [0, 0, 0]; // Return some default value or handle the error appropriately
+      return [0, 0, 0];
     }
     const satrec = satellite.twoline2satrec(line1, line2); // Initializing the satellite record with the TLE (line 1 and line 2)
-    // console.log(satrec)
     var date = new Date();
 
-    //date = new Date (date.getTime() + 800000); // <-- TEST CODE (DO NOT UNCOMMENT THE CODE IF YOU DONT KNOW WHAT YOU ARE DOING)
-
-    // Getting the position of the satellite at the given date
     // The position_velocity result is a key-value pair of ECI coordinates.
-    // https://celestrak.org/columns/v02n01/#:~:text=The%20ECI%20coordinate%20system%20(see,orthogonal%20(mutually%20perpendicular)%20axes.
-
-    var positionAndVelocity = satellite.propagate(satrec, date);
-    // console.log(positionAndVelocity);
+    // These are the base results from which all other coordinates are derived.
+    var positionAndVelocity = satellite.propagate(
+      satrec,
+      new Date(date.getTime() + 0)
+    );
 
     // grabbing GMST for the coordinate transforms.
     // https://en.wikipedia.org/wiki/Sidereal_time#Definition
 
-    const gmst = satellite.gstime(date);
-    // console.log(gmst);
-
-    // converts Earth-centered inertial ECI coordinates, specified by position, to latitude, longitude, altitude (LLA) geodetic coordinates.
+    const gmst = satellite.gstime(new Date(date.getTime() + 0));
+    // converts Earth-centered inertial ECI coordinates, specified by position, to (latitude, longitude, altitude [LLA]) geodetic coordinates.
+    positionEcf = satellite.eciToEcf(
+      positionAndVelocity.position,
+      satellite.gstime(new Date(date.getTime() + 0))
+    );
     const positionGd = satellite.eciToGeodetic(
       positionAndVelocity.position,
       gmst
     );
-    // console.log(positionGd);
 
     // Converting the RADIANS to DEGREES (given the results were in radians)
     const long = (180 * positionGd.longitude) / Math.PI;
     const lat = (180 * positionGd.latitude) / Math.PI;
     const height = positionGd.height;
-    //console.log([long, lat]);
     return [long, lat, height];
+  };
+
+  const calcPasses = (line1, line2, observerCoords) => {
+    const satrec = satellite.twoline2satrec(line1, line2);
+    const passes = [];
+    let currentPass = null;
+    let peakElevation = -Infinity;
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() + 10);
+
+    for (
+      let date = new Date();
+      date <= endDate;
+      date.setSeconds(date.getSeconds() + 10)
+    ) {
+      const positionAndVelocity = satellite.propagate(satrec, date);
+      const gmst = satellite.gstime(date);
+      const positionEcf = satellite.eciToEcf(
+        positionAndVelocity.position,
+        gmst
+      );
+      const positionGd = satellite.eciToGeodetic(
+        positionAndVelocity.position,
+        gmst
+      );
+      const lookAngles = satellite.ecfToLookAngles(observerCoords, positionEcf);
+
+      const azimuth = satellite.radiansToDegrees(lookAngles.azimuth);
+      const elevation = satellite.radiansToDegrees(lookAngles.elevation);
+
+      if (elevation > 5) {
+        if (!currentPass) {
+          currentPass = {
+            startAzimuth: azimuth,
+            startElevation: elevation,
+            startTime: new Date(date),
+            peakAzimuth: azimuth,
+            peakElevation: elevation,
+            peakTime: new Date(date),
+            endAzimuth: azimuth,
+            endElevation: elevation,
+            endTime: new Date(date),
+          };
+          peakElevation = elevation;
+        } else {
+          if (elevation > peakElevation) {
+            currentPass.peakAzimuth = azimuth;
+            currentPass.peakElevation = elevation;
+            currentPass.peakTime = new Date(date);
+            peakElevation = elevation;
+          }
+          currentPass.endAzimuth = azimuth;
+          currentPass.endElevation = elevation;
+          currentPass.endTime = new Date(date);
+        }
+      } else if (currentPass) {
+        if (currentPass.peakElevation >= 10) {
+          passes.push(currentPass);
+        }
+        currentPass = null;
+        peakElevation = -Infinity;
+      }
+    }
+    console.log(passes);
   };
 
   const path = (line1, line2) => {
@@ -58,9 +124,12 @@ const Calc = ({ satNum }) => {
     var i = 0;
 
     //console.log(date);
-    for (; i < 3600; i++) {
-      var positionAndVelocity = satellite.propagate(satrec, date);
-      const gmst = satellite.gstime(date);
+    for (; i < 5000; i++) {
+      var positionAndVelocity = satellite.propagate(
+        satrec,
+        new Date(date.getTime() + 0)
+      );
+      const gmst = satellite.gstime(new Date(date.getTime() + 0));
       const positionGd = satellite.eciToGeodetic(
         positionAndVelocity.position,
         gmst
@@ -76,9 +145,12 @@ const Calc = ({ satNum }) => {
       date = new Date(date.getTime() + 1000);
     }
 
-    for (var j = 0; j < 3600 - i; j++) {
-      var positionAndVelocity = satellite.propagate(satrec, date);
-      const gmst = satellite.gstime(date);
+    for (var j = 0; j < 5000 - i; j++) {
+      var positionAndVelocity = satellite.propagate(
+        satrec,
+        new Date(date.getTime() + 0)
+      );
+      const gmst = satellite.gstime(new Date(date.getTime() + 0));
       const positionGd = satellite.eciToGeodetic(
         positionAndVelocity.position,
         gmst
@@ -126,18 +198,26 @@ const Calc = ({ satNum }) => {
       );
     }
   };
+  const CalcPass = () => {
+    if (satellites?.tle && satellites.tle.length === 2) {
+      const s = satellites.tle[0];
+      const p = satellites.tle[1];
+      const data = calcPasses(satellites.tle[0], satellites.tle[1], observerGd);
+    }
+  };
 
   useEffect(() => {
+    CalcPass();
     CalcCoords();
     const intervalId1 = setInterval(CalcCoords, 2000);
-    return () => clearInterval(intervalId1);
-  }, [satellites?.tle]);
-
-  useEffect(() => {
     CalcPath();
     const intervalId2 = setInterval(CalcPath, 5000);
-    return () => clearInterval(intervalId2);
+    return () => {
+      clearInterval(intervalId2);
+      clearInterval(intervalId1);
+    };
   }, [satellites?.tle]);
+
   return;
 };
 
