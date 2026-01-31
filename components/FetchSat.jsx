1d67c71086785557bbbe7536daa28eb92d622978
Utsav Poddar <utsavpoddar002@gmail.com>
2026-01-31 20:35:20 +0530

    Update API endpoint for fetching satellite data

--- components/FetchSat.jsx
+++ components/FetchSat.jsx
@@ -25,7 +25,7 @@ const FetchSat = ({ satNum }) => {
     try {
       const [tleResponse] = await Promise.all([
         axios.get(
-          `https://satellite-backend-v1.onrender.com/${satNum}`
+          `https://satellite-backend-lo09.onrender.com/${satNum}`
         ),
       ]);
 
